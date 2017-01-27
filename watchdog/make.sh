98bb80ec7daf88b505de46dad20328fd556ebd39
Guenter Roeck <linux@roeck-us.net>
2017-01-27 06:36:00 -0800

    watchdog/make: Various improvements
    Most importantly, drop all clk_enable_prepare related patches.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- watchdog/make.sh
+++ watchdog/make.sh
@@ -23,6 +23,9 @@ run ../common/clkreturn
 run ../common/clk2
 run ../common/clk
 run ../common/of_iomap
+# run ../common/devm_kzalloc
+# run ../common/kzalloc
+run ../common/action
 # Only for iTCO, which we drop anyway
 # run ../common/ioremap
 # Done manually: use watchdog core
@@ -32,10 +35,11 @@ run ../common/of_iomap
 run ../common/mutex_destroy
 run ../common/worker
 run ../common/goto
-# This benefits from a second run
-run ../common/goto
 run ../common/pdata
 run ../common/pdev
+run ../common/pdev-addvar
+run ../common/cleanup
+run ../common/goto
 run ../common/cleanup
 
 if [ -n "${noclean}" ]
@@ -43,24 +47,59 @@ then
     exit 0
 fi
 
+cleanup()
+{
+	if [ -e $1 ]
+	then
+		rm $1; git checkout $1
+	fi
+}
+
+# drop all patches associated with clk_disable_unprepare
+# (courtesy C standard)
+
+cleanup drivers/watchdog/asm9260_wdt.c
+cleanup drivers/watchdog/at91sam9_wdt.c
+cleanup drivers/watchdog/atlas7_wdt.c
+cleanup drivers/watchdog/bcm7038_wdt.c
+cleanup drivers/watchdog/cadence_wdt.c
+cleanup drivers/watchdog/coh901327_wdt.c
+cleanup drivers/watchdog/davinci_wdt.c
+cleanup drivers/watchdog/dw_wdt.c
+cleanup drivers/watchdog/imgpdc_wdt.c
+cleanup drivers/watchdog/imx2_wdt.c
+cleanup drivers/watchdog/loongson1_wdt.c
+cleanup drivers/watchdog/lpc18xx_wdt.c
+cleanup drivers/watchdog/meson_gxbb_wdt.c
+cleanup drivers/watchdog/of_xilinx_wdt.c
+cleanup drivers/watchdog/orion_wdt.c
+cleanup drivers/watchdog/pic32-dmt.c
+cleanup drivers/watchdog/pic32-wdt.c
+cleanup drivers/watchdog/pnx4008_wdt.c
+cleanup drivers/watchdog/qcom-wdt.c
+cleanup drivers/watchdog/st_lpc_wdt.c
+cleanup drivers/watchdog/sunxi_wdt.c
+cleanup drivers/watchdog/tangox_wdt.c
+cleanup drivers/watchdog/txx9wdt.c
+
 # The following patches are known to be broken, problematic, or cosmetic
 
 # cpufreq interaction
-rm drivers/watchdog/s3c2410_wdt.c; git checkout drivers/watchdog/s3c2410_wdt.c
+cleanup drivers/watchdog/s3c2410_wdt.c
 
 # static struct ie6xx_wdt_data should really be allocated
-rm drivers/watchdog/ie6xx_wdt.c; git checkout drivers/watchdog/ie6xx_wdt.c
+cleanup drivers/watchdog/ie6xx_wdt.c
 
 # possible impact from pm_runtime functions
 #	drivers/watchdog/renesas_wdt.c; git checkout drivers/watchdog/renesas_wdt.c
 #	drivers/watchdog/shwdt.c; git checkout drivers/watchdog/shwdt.c
 
 # uses miscdevice, don't bother
-rm drivers/watchdog/ath79_wdt.c; git checkout drivers/watchdog/ath79_wdt.c
-rm drivers/watchdog/ar7_wdt.c; git checkout drivers/watchdog/ar7_wdt.c
-rm drivers/watchdog/cpwd.c; git checkout drivers/watchdog/cpwd.c
-rm drivers/watchdog/gef_wdt.c; git checkout drivers/watchdog/gef_wdt.c
-rm drivers/watchdog/at91rm9200_wdt.c; git checkout drivers/watchdog/at91rm9200_wdt.c
-rm drivers/watchdog/riowd.c; git checkout drivers/watchdog/riowd.c
-rm drivers/watchdog/sch311x_wdt.c; git checkout drivers/watchdog/sch311x_wdt.c
-rm drivers/watchdog/sp5100_tco.c; git checkout drivers/watchdog/sp5100_tco.c
+cleanup drivers/watchdog/ath79_wdt.c
+cleanup drivers/watchdog/ar7_wdt.c
+cleanup drivers/watchdog/cpwd.c
+cleanup drivers/watchdog/gef_wdt.c
+cleanup drivers/watchdog/at91rm9200_wdt.c
+cleanup drivers/watchdog/riowd.c
+cleanup drivers/watchdog/sch311x_wdt.c
+cleanup drivers/watchdog/sp5100_tco.c
