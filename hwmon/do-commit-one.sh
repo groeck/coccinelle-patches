4d1702f9bc420781beef4e8fdae42e861cee2256
Guenter Roeck <linux@roeck-us.net>
2019-01-21 20:22:13 -0800

    hwmon: Split do-commit into two files
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

---
+++ hwmon/do-commit-one.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+basedir="$(cd $(dirname $0); pwd)"
+
+maintainers()
+{
+    local file="$1"
+    local tmpfile="$(mktemp)"
+    local m
+
+    cc=""
+
+    scripts/get_maintainer.pl --no-l --no-rolestats ${file} | \
+	egrep -v "Roeck|Delvare" > ${tmpfile}
+
+    while read -r m; do
+	cc="${cc}
+Cc: $m"
+    done < ${tmpfile}
+
+    rm -f ${tmpfile}
+}
+
+handle_one()
+{
+    local a="$1"
+    local objfile="${a/%.c/.o}"
+    local obasename="$(basename ${objfile})"
+
+    echo "Handling $a"
+
+    if [[ ! -e "${objfile}" ]]; then
+	echo "$a: No object file. Skipping."
+	return 0
+    fi
+
+    size1="$(cd drivers/hwmon; size ${obasename})"
+    size2="$(cd drivers/hwmon/old; size ${obasename})"
+
+    if [[ "${size1}" != "${size2}" ]]; then
+	echo "${objbasename}: Object file size mismatch. Skipping."
+	return 0
+    fi
+
+    if egrep -q 'SENSOR_ATTR\(|SENSOR_ATTR_2\(' $a; then
+	echo "Warning: $a contains unconverted SENSOR_ATTR macros. Skipping."
+	return 0
+    fi
+    if egrep -q 'SENSOR_DEVICE_ATTR\(|SENSOR_DEVICE_ATTR_2\(' $a; then
+	echo "Warning: $a contains unconverted SENSOR_DEVICE_ATTR macros. Skipping."
+	return 0
+    fi
+    if egrep -q "S_IR|S_IW" $a; then
+	echo "Warning: $a contains permission defines. Skipping."
+	return 0
+    fi
+
+    # Coccinelle sometimes leaves eight spaces instead of tabs
+    sed -i -e 's/	        /		/' $a
+    git add $a
+
+    maintainers $a
+    sensordev=0
+    if egrep -q "SENSOR_ATTR|SENSOR_DEVICE_ATTR" $a; then
+	sensordev=1
+    fi
+
+    if [[ "${sensordev}" -ne 0 ]]; then
+        subject="Use permission specific SENSOR[_DEVICE]_ATTR variants"
+        msg="Use SENSOR[_DEVICE]_ATTR[_2]_{RO,RW,WO} to simplify the source code,
+to improve readbility, and to reduce the chance of inconsistencies.
+
+Also replace any remaining S_<PERMS> in the driver with octal values."
+    else
+        subject="Replace S_<PERMS> with octal values"
+	msg="Replace S_<PERMS> with octal values."
+    fi
+    msg+="
+
+The conversion was done automatically with coccinelle. The semantic patches
+and the scripts used to generate this commit log are available at
+https://github.com/groeck/coccinelle-patches/hwmon/.
+
+This patch does not introduce functional changes. It was verified by
+compiling the old and new files and comparing text and data sizes.
+"
+    git commit -s \
+	-m "hwmon: ($(basename -s .c $a)) ${subject}" \
+	-m "${msg}" \
+-m "${cc}"
+}
--- hwmon/do-commit.sh
+++ hwmon/do-commit.sh
@@ -1,41 +1,10 @@
-basedir=$(cd $(dirname $0); pwd)
+#!/bin/bash
 
-maintainers()
-{
-    local file=$1
-    local tmpfile=$(mktemp)
-    local m
+basedir="$(cd $(dirname $0); pwd)"
 
-    cc=""
+. ${basedir}/do-commit-one.sh
 
-    scripts/get_maintainer.pl --no-l --no-rolestats ${file} | \
-	egrep -v "Guenter Roeck|Delvare" > ${tmpfile}
-
-    while read -r m; do
-	cc="${cc}
-Cc: $m"
-    done < ${tmpfile}
-
-    rm -f ${tmpfile}
-}
-
-git status | grep modified: | awk '{print $2}' | while read a
-do
-    echo "Handling $a"
-    git add $a
-
-    maintainers $a
-    subject="Convert to use SENSOR_DEVICE_ATTR[_2]_{RO,RW,WO}"
-    msg="Auto-convert to use SENSOR_DEVICE_ATTR[_2]_{RO,RW,WO}
-to simplify the source code, improve readbility, and reduce the chance of
-inconsistencies. As a side effect, this conversion eliminates the use of
-S_<PERMS> in the driver.
-"
-    git commit -s \
-	-m "hwmon: ($(basename -s .c $a)) ${subject}" \
-	-m "${msg}" \
-	-m "The conversion was done automatically with coccinelle. The semantic patches
-and the scripts used to generate this commit log are available at
-https://github.com/groeck/coccinelle-patches/hwmon/" \
--m "${cc}"
+files=$(git status | grep modified: | awk '{print $2}')
+for file in ${files}; do
+    handle_one ${file}
 done
